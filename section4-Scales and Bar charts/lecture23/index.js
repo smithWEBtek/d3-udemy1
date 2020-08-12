32790a346495fbd0b44c78929d86d6e802c29215
github username <github email address>
2020-08-11 20:58:35 -0400

    Finish lecture 23

--- section4-Scales and Bar charts/lecture23/index.js
+++ section4-Scales and Bar charts/lecture23/index.js
@@ -1,40 +1,30 @@
-// select svg container
-// const colors = ['blue', '#556677', 'pink', 'red', 'green', 'yellow', 'violet']
-
+// select the svg container first
 const svg = d3.select('svg')
 
 d3.json('menu.json').then(data => {
 
-	const y = d3.scaleLinear()
-		.domain([0, 1000])
-		.range([0, 500]);
+  const y = d3.scaleLinear()
+    .domain([0, 1000])
+    .range([0, 500])
 
-	const x = d3.scaleBand()
-		.domain(data.map(item => item.name))
-		.range([0, 500])
-		.paddingInner(0.1)
-		.paddingOuter(0.2);
+  const x = d3.scaleBand()
+    .domain(data.map(item => item.name))
+    .range([0, 400])
+    .paddingInner(0.2)
+    .paddingOuter(0.2)
 
+  const rects = svg.selectAll('rect')
+    .data(data)
 
+  rects.attr('width', x.bandwidth)
+    .attr('height', d => y(d.orders))
+    .attr('fill', 'orange')
+    .attr('x', d => x(d.name))
 
-	// join data to rects and select all 'rect's from DOM, 
-	// (if data requires more 'rect's than exist, d3 will create virtual ones next)
-	const rects = svg.selectAll('rect')
-		.data(data)
-
-	rects.attr('width', x.bandwidth)
-		.attr('height', d => y(d.orders))
-		.attr('fill', 'orange')
-		.attr('x', d => x(d.name));
-
-	rects.enter()
-		.append('rect')
-		.attr('width', x.bandwidth)
-		.attr('height', d => y(d.orders))
-		.attr('fill', () => {
-			return generateColor()
-		})
-		.attr('x', d => x(d.name));  // passing the name value through the x scale, to return an x coordinate value
-
-})
-
+  rects.enter()
+    .append('rect')
+    .attr('height', d => y(d.orders))
+    .attr('fill', 'orange')
+    .attr('width', x.bandwidth)
+    .attr('x', d => x(d.name))
+}) 
